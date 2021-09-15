80683aefece8965ffd4ab4c726a0893708ac22f3
Offgridhuls <55862297+Offgridhuls@users.noreply.github.com>
2021-09-15 14:59:55 -0400

    Initial commit

---
+++ Assets/[Scripts]/StartButtonBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed");
+    }
+
+}
